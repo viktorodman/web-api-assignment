54f4232d0a92f1bef662dc9b891234cbfa34d5db
Viktor Ödman <vo222dq@student.lnu.se>
2021-02-27 20:02:42 +0100

    Updating user model

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -1,13 +1,49 @@
 import { NextFunction, Request, Response } from "express";
 
+import User, { IUser } from '../models/user'
 
 export default class UserController {
 
-    getAllUsers(req: Request, res: Response, next: NextFunction): void {
+    public getAllUsers(req: Request, res: Response, next: NextFunction): void {
         res.json('From get all users')
     }
 
-    createUser(req: Request, res: Response, next: NextFunction): void {
-        res.json('From Create User')
+    public async createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const username = this.getRequestUsername(req)
+            const password = this.getRequestPassword(req)
+
+            const user = await User.create({
+                username,
+                password
+            })
+            res.json(`Created User: ${user.username}`)
+        } catch (error) {
+            console.log(res)
+            if (error.code === 11000) {
+                res.json({
+                    status: 409,
+                    message: 'User already registered'
+                })
+            } else if (error.name === 'ValidationError') {
+                console.log(error.message)
+                res.json({
+                    status: 400,
+                    message: error.message
+                })
+            } else {
+                console.log(error.name)
+                res.json({error})
+            }
+        }
+       
+    }
+
+    private getRequestUsername(req: Request): string {
+        return req.body.username
+    }
+
+    private getRequestPassword(req: Request): string {
+        return req.body.password
     }
 }
--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,14 +1,30 @@
 import * as mongoose from 'mongoose'
+/* import * as validator from 'validator' */
+import * as bcrypt from 'bcrypt'
 
 export interface IUser extends mongoose.Document {
-    name: string
+    username: string
     password: string
 }
 
 export const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true , unique: true},
-    password: { type: String, required: true , unique: true}
+    username: { 
+        type: String,
+        required: [true, 'Username is missing'], 
+        unique: true
+    },
+    password: { 
+        type: String, 
+        minlength: [10, 'Password must be at least 10 characters'],
+        required: [true, 'Password is required']
+    }
+}, {timestamps: true}
+)
+
+UserSchema.pre<IUser>('save', async function () {
+    this.password = await bcrypt.hash(this.password, 10)
 })
+
 
 const User = mongoose.model<IUser>('User', UserSchema)
 
