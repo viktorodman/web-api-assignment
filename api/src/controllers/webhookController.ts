285582ec664898981cd8047a855073f170ab926a
Viktor Ödman <vo222dq@student.lnu.se>
2021-03-02 22:01:00 +0100

    Updating webhookController

--- api/src/controllers/webhookController.ts
+++ api/src/controllers/webhookController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import UserRequest from "interfaces/IUserRequest";
-import Hook from "../models/hook";
+import Hook, { IHook } from "../models/hook";
 
 
 
@@ -10,23 +10,44 @@ export default class WebhookController {
         try {
             const hooks = await Hook.getAllUserHooks(req.user.username)
 
-            console.log(hooks)
-            res.status(200).json('asdföa')
+            const data = {
+                _links: {
+                    self: { href: `${req.protocol}://${req.get('host')}${req.originalUrl}` }
+                }, 
+                size: hooks.length,
+                user_hooks: hooks.map(hook => this.createHookResponseObject(hook, req))
+            }
+            res.status(200).json(data)
         } catch (error) {
             next(error)
         }
     }
-
+    
     public async registerHook(req, res: Response, next: NextFunction): Promise<void> {
         try {
             const hook = await Hook.create(this.getRequestHookDetails(req))
-
+            
             res.json(hook)
         } catch (error) {
             next(error)
         }
     }
 
+    private createHookResponseObject(hook: IHook, req: any): any {
+        const { _id, user, url, catch_event, catch_event_lake_filter, catch_event_species_filter } = hook
+        return {
+            _links: {
+                self: { href: `${req.protocol}://${req.get('host')}${req.originalUrl}/${hook._id}`} 
+            },
+            id: _id, 
+            user,
+            url, 
+            catch_event,
+            catch_event_lake_filter, 
+            catch_event_species_filter 
+        }
+    }
+    
     private getRequestHookDetails(req: any): any {
         const { url, catch_event, catch_event_lake_filter, catch_event_species_filter } = req.body
         return {
