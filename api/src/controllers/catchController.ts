9f011d1a72bf5447165967c60b8b86d7b48f9f8e
Viktor Ödman <vo222dq@student.lnu.se>
2021-03-02 09:28:29 +0100

    Updating catchController

--- api/src/controllers/catchController.ts
+++ api/src/controllers/catchController.ts
@@ -6,7 +6,26 @@ export default class CatchController {
 
     public async getAllCatches(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            res.status(200).json("Fishshshsh")
+            const catches = await Catch.getAll()
+
+            const data = {
+                _links: {
+                    self: { href: `${req.protocol}://${req.get('host')}${req.originalUrl}` }
+                },
+                size: catches.length,
+                _embedded: {
+                    catches: catches.map(c => ({
+                        _links: {
+                            self: { href: `${req.protocol}://${req.get('host')}${req.originalUrl}/${c._id}` }
+                        },
+                        fisher: c.fisher, 
+                        fishSpecies: c.fishSpecies,
+                        measurement: c.measurement,
+                        location: c.location
+                    }))
+                }
+            }
+            res.status(200).json(data)
         } catch (error) {
             res.status(error.status).json({
                 status: error.status,
@@ -23,7 +42,7 @@ export default class CatchController {
                 fisher: req.user.username,
                ...req.body
             })
-            res.status(201).json(fishCatch)
+            res.status(201).json(fishCatch._id)
         } catch (error) {
             res.json(error)
         }
